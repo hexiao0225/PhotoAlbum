ead8e4f09aad379e0cc58ad1237b0658553dbd0c
Xiao He <hexiao0225@gmail.com>
2020-06-11 16:00:00 -0400

    initial setup

---
+++ molene/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./styles/style.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>hi</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
