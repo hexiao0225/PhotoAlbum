5953e2aedc3323abaa9623c5eb1d4a13fcc9ae8a
Xiao He <hexiao0225@gmail.com>
2020-06-17 11:00:04 -0400

    Updates

---
+++ precache-manifest.7710604bcc071d38ab01a04fa259ed59.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10c283f4ec41f8d217d0ea99eaaa3181",
+    "url": "/hexiao0225/PhotoAlbum/index.html"
+  },
+  {
+    "revision": "8178f1803ac3a29d3fbb",
+    "url": "/hexiao0225/PhotoAlbum/static/css/main.57a5fb1c.chunk.css"
+  },
+  {
+    "revision": "b754f2c354b212ea1741",
+    "url": "/hexiao0225/PhotoAlbum/static/js/2.07e76d66.chunk.js"
+  },
+  {
+    "revision": "236a9cef38ee70c895f699466f1ff3bf",
+    "url": "/hexiao0225/PhotoAlbum/static/js/2.07e76d66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8178f1803ac3a29d3fbb",
+    "url": "/hexiao0225/PhotoAlbum/static/js/main.83bad2ae.chunk.js"
+  },
+  {
+    "revision": "36034083ee4547f48b13",
+    "url": "/hexiao0225/PhotoAlbum/static/js/runtime-main.40b0d320.js"
+  },
+  {
+    "revision": "61aec1412c3866de298757b470f9dad8",
+    "url": "/hexiao0225/PhotoAlbum/static/media/DINPro-Bold tr.61aec141.woff"
+  },
+  {
+    "revision": "1133030c9bdf2fe8d44474059effd22d",
+    "url": "/hexiao0225/PhotoAlbum/static/media/DINPro-Light tr.1133030c.woff"
+  },
+  {
+    "revision": "bbed362ea4f479c32fe2044f100f6e53",
+    "url": "/hexiao0225/PhotoAlbum/static/media/DINPro-Medium tr.bbed362e.woff"
+  },
+  {
+    "revision": "d521e1a9fd8c03580a80817849f0f572",
+    "url": "/hexiao0225/PhotoAlbum/static/media/Portrait-script-regular.d521e1a9.ttf"
+  }
+]);
